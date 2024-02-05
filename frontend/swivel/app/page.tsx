d90e517e0b6993156635ba5bfd7a78db29cb831f
Kaikei-e <e_kaikei@protonmail.com>
2024-02-05 23:10:47 +0900

    Making the landing page

--- frontend/swivel/app/page.tsx
+++ frontend/swivel/app/page.tsx
@@ -1,11 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <div className="-z-50">
+        <Image
+          src={"/flower_bells.jpg"}
+          alt={"blue small blowers on the ground"}
+          fill
+          quality={100}
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.appStatement}>
+        <p>Swivel: Manage your balance in the smartest way possible.</p>
       </div>
     </main>
-  )
+  );
 }
